197db60ecf7f532ce3bb2d964895f1b661a1bbc9
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-10-03 17:55:40 -0500

    modificando layout

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -7,12 +7,10 @@ const DashBoard = () => {
     return (
         <section className={styles.dashboard}>
             <div className={styles.dashboard_layout}>
-                <section className={styles.userDetails}>
+                <div className={styles.details}>
                     <UserProfile/>
-                </section>
-                <section className={styles.timeTracking}>             
-                    <Tracking/>
-                </section>
+                </div>
+                <Tracking/>
             </div>
         </section>
     )
