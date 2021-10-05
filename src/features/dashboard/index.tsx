6c7a8ec33c1b4030c8b4519a7f83faa6db570e8b
Roberto Carlos Galicia Lopez <dev.robertgl@gmail.com>
2021-10-05 00:07:14 -0500

    agregando context provider

--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,18 +1,20 @@
 import Tracking from './components/Tracking';
 import UserProfile from './components/UserProfile';
 import styles from './css/dashboard.module.css';
-
+import { TrackingProvider } from './context/TrackingProvaider';
 
 const DashBoard = () => {
     return (
-        <section className={styles.dashboard}>
-            <div className={styles.dashboard_layout}>
-                <div className={styles.details}>
-                    <UserProfile/>
+        <TrackingProvider>
+            <section className={styles.dashboard}>
+                <div className={styles.dashboard_layout}>
+                    <div className={styles.details}>
+                        <UserProfile/>
+                    </div>
+                    <Tracking/>
                 </div>
-                <Tracking/>
-            </div>
-        </section>
+            </section>
+        </TrackingProvider>
     )
 }
 
